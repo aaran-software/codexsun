e03575959f73fc5e113eb43b3df8e5a8cc544fca
SUNDAR <sundar@codexsun.com>
2025-10-01 18:52:12 +0530

    #25 restructuring code base

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,14 +1,18 @@
 import {ThemeProvider} from "@/components/theme/theme-provider.tsx";
 import {ModeToggle} from "@/components/theme/mode-toggle.tsx";
+import AppLayout from "@/components/layouts/app-layout.tsx";
 
 function App() {
     return (
         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-            <ModeToggle/>
-            <p className="text-3xl font-bold underline bg-amber-300 p-4">
-                Click on the Vite and React logos to learn more
-            </p>
+            <AppLayout>
+                <ModeToggle/>
+                <p className="text-3xl font-bold underline bg-amber-300 p-4">
+                    Click on the Vite and React logos to learn more
+                </p>
+            </AppLayout>
         </ThemeProvider>
     )
 }
+
 export default App
