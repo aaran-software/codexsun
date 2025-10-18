aef862bf55655369410d355178cd8285041347bb
SUNDAR <sundar@codexsun.com>
2025-10-18 12:45:40 +0530

    #94 reworking on frontend user

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     },
     server: {
         host: '0.0.0.0', // ← required for Docker container to be accessible externally
-        port: 3006,      // ← choose your desired port
+        port: 5006,      // ← choose your desired port
         strictPort: true // ← fails if port is already in use
     },
 })
