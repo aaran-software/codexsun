4fedb6ff916c346b3cd274595976275fa801df50
SUNDAR <sundar@codexsun.com>
2025-10-24 08:21:37 +0530

    #95 working on connectivity issues

--- apps/backend/test-db.ts
+++ apps/backend/test-db.ts
@@ -1,14 +1,20 @@
+import {getMasterDbConfig} from "./cortex/config/db-config";
+
 const mariadb = require('mariadb');
+
+const config = getMasterDbConfig()
+
+
 async function test() {
     try {
         console.log('🧪 TESTING DIRECT CONNECTION...');
         const pool = mariadb.createPool({
-            host: 'mariadb',
-            port: 3306,
-            user: 'root',
-            password: 'DbPass1@@',
-            connectionLimit: 50,
-            acquireTimeout: 10000
+            host: config.host,
+            port: config.port,
+            user: config.user,
+            password: config.password,
+            connectionLimit: config.connectionLimit,
+            acquireTimeout: config.acquireTimeout,
         });
 
         const conn = await pool.getConnection();
