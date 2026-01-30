678e9c74ed369e322688f3c6bb3234e62dcb4604
Sundar-office <sundar@sundar.com>
2026-01-30 20:15:27 +0530

    #6 working for update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->configureDefaults();
 
-        if (app()->environment('production')) {
+        if (app()->environment('local')) {
             URL::forceScheme('https');
         }
     }
