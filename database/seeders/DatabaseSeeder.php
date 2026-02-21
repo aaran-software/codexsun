6a74eabe584993f7a6fc2b165fcb156c472fe7ea
SUNDAR <sundar@codexsun.com>
2026-02-21 20:23:44 +0530

    #8 working on tenant

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,36 @@
 
 namespace Database\Seeders;
 
+use Aaran\Tenant\Database\Seeders\TenantSeeder;
+use Aaran\Tenant\Database\Seeders\ThemePresetSeeder;
+use Aaran\Tenant\Database\Seeders\ThemeSeeder;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'sundar',
+            'email' => 'sundar@sundar.com',
+            'password' => Hash::make('kalarani'),
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+            'two_factor_secret' => null,
+            'two_factor_recovery_codes' => null,
+            'two_factor_confirmed_at' => null,
+        ]);
+
+
+        $this->call([
+            TenantSeeder::class,
+            ThemePresetSeeder::class,
+            ThemeSeeder::class,
         ]);
     }
 }
