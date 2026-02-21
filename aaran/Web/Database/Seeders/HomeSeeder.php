683c91c6fee79e60187ad577dac771480ebeb1f4
SUNDAR <sundar@codexsun.com>
2026-02-21 08:55:38 +0530

    #3 working on home page

---
+++ aaran/Web/Database/Seeders/HomeSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Aaran\Web\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class HomeSeeder extends Seeder
+{
+    public function run(): void
+    {
+        //
+    }
+}
